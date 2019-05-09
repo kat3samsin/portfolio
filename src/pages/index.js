d8737d5354845cc76d82db03445ba29fd18e9a1a
katre <kat3samsin@gmail.com>
2019-05-10 03:12:05 +0800

    updated description

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,6 @@ const IndexPage = () => (
     <SEO title="Katrina Lou S. Tantay" keywords={[`katrina`, `lou`, `samsin`, 'tantay', 'gatsby', 'portfolio']} />
     <h1>Hi I'm Katre!</h1>
     <p>Welcome to my portfolio.</p>
-    <p>Hope you have fun!</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
